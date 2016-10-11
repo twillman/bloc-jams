7e6df853c3010bb9697ea589d318c4e919fea300
Tara Willman <tara.willman@gmail.com>
2016-10-10 22:42:14 -0700

    Dom Scripting Events Checkpoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
-      var points = document.getElementsByClassName('point');
+
       var revealPoint = function(index){
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
@@ -13,3 +14,16 @@ var animatePoints = function() {
      }
 
  };
+
+window.onload = function() {
+  if (winddow.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener("scroll", function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
